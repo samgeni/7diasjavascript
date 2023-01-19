a1e3b3a03ffd5aa232f081d813efcf5bbb333d53
Igor Borges <ibcastelheira1995@gmail.com>
2023-01-19 20:07:03 -0300

    primeiro commit

---
+++ hg.js
@@ -0,0 +1,8 @@
+let productvalue = Number(prompt('enter the product value'))
+if (productvalue >= 20) {
+    document.getElementById('result').innerHTML = ''
+    alert('aprovado')
+} else {
+    document.getElementById('result').innerHTML = ''
+    alert('negado')
+}
